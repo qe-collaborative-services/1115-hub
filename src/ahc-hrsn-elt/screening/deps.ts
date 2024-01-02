6ca6c6e35cf119325e0f9c6382877d886ee153f3
Shahid N. Shah <shahid@shah.org>
2024-01-02 16:39:54 -0500

    chore: update deps

--- src/ahc-hrsn-elt/screening/deps.ts
+++ src/ahc-hrsn-elt/screening/deps.ts
@@ -8,12 +8,12 @@ export * as dax from "https://deno.land/x/dax@0.36.0/mod.ts";
 export * as safeText from "npm:string-ts";
 
 // local: ../../../../../netspective-labs/sql-aide
-// remote: https://raw.githubusercontent.com/netspective-labs/sql-aide/x0.11.18
+// remote: https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.11.19
 
-export * as array from "../../../../../netspective-labs/sql-aide/lib/universal/array.ts";
-export * as chainNB from "../../../../../netspective-labs/sql-aide/lib/notebook/chain-of-responsibility.ts";
-export * as duckdb_shell from "../../../../../netspective-labs/sql-aide/lib/duckdb/mod.ts";
-export * as ws from "../../../../../netspective-labs/sql-aide/lib/universal/whitespace.ts";
+export * as array from "https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.11.19/lib/universal/array.ts";
+export * as chainNB from "https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.11.19/lib/notebook/chain-of-responsibility.ts";
+export * as duckdb_shell from "https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.11.19/lib/duckdb/mod.ts";
+export * as ws from "https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.11.19/lib/universal/whitespace.ts";
 
-export * as SQLa from "../../../../../netspective-labs/sql-aide/render/mod.ts";
-export * as SQLa_ingest_duckdb from "../../../../../netspective-labs/sql-aide/pattern/ingest/duckdb/mod.ts";
+export * as SQLa from "https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.11.19/render/mod.ts";
+export * as SQLa_ingest_duckdb from "https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.11.19/pattern/ingest/duckdb/mod.ts";
