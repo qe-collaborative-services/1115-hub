55e88db6acecd7e6a9d2abb0134b9ab3aeefaa0a
shajukochappu <shajukochappu@citrusinformatics.com>
2024-04-09 17:10:26 +0530

    chore: upgrade to version 0.12.2

--- src/ahc-hrsn-elt/screening/orchestrate.ts
+++ src/ahc-hrsn-elt/screening/orchestrate.ts
@@ -17,7 +17,7 @@ import * as csv from "./csv.ts";
 import * as excel from "./excel.ts";
 import * as gov from "./governance.ts";
 
-export const ORCHESTRATE_VERSION = "0.12.1";
+export const ORCHESTRATE_VERSION = "0.12.2";
 
 export interface FhirRecord {
   PAT_MRN_ID: string;
