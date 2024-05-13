b59a9003ed4154c451e23475bfcb39d3396f2cac
Vinod <vinod-aravindakshan@netspective.in>
2024-05-13 19:19:51 +0530

    fix: revert FHIRApplication without dynamic host and port options

--- src/service/fhir-server-prime/src/main/java/org/techbd/service/http/FHIRApplication.java
+++ src/service/fhir-server-prime/src/main/java/org/techbd/service/http/FHIRApplication.java
@@ -1,42 +1,18 @@
 package org.techbd.service.http;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.server.ConfigurableWebServerFactory;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-
-import java.net.InetAddress;
-import java.util.Collections;
 
 @SpringBootApplication
-public class FHIRApplication implements ApplicationRunner, WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
-
-	@Value("${server.port}")
-	private int port;
-
-	@Value("${server.host}")
-	private InetAddress host;
+public class FHIRApplication {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(FHIRApplication.class);
-		app.setDefaultProperties(Collections
-				.singletonMap("server.port", "8080"));
-		app.setDefaultProperties(Collections.singletonMap("server.host", "localhost"));
-		app.run(args);
-	}
-
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-
-	}
-
-	@Override
-	public void customize(ConfigurableWebServerFactory factory) {
-		factory.setPort(port);
-		factory.setAddress(host);
+		try {
+			SpringApplication.run(FHIRApplication.class, args);
+			// Code that may throw an exception
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 
 }
